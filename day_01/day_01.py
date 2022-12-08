2a25fd1f385e1f0a0c4505c633e63a305f0eae88
mattblas <matt.blaszkowski@gmail.com>
2022-12-08 18:52:42 +0100

    day 01

--- day_01/day_01.py
+++ day_01/day_01.py
@@ -1,20 +1,28 @@
 
 def main():
-    get_input()
     with open("Calories.txt", "r") as f:
         Calories = list(f)[0]
-        elves_split = Calories.split("/n/n")
+        elves_split = Calories.split("\\n\\n")
+        # print(elves_split)
         _ = 1
         dict = {}
         for elv in elves_split:
-            item_split = elv.split("/n")
+            item_split = elv.split("\\n")
             int_item_split = list(map(int, item_split))
             items_sum = sum(int_item_split)
             dict[_] = items_sum
             _+=1
-        Elf_with_max_cal = max(dict, key=dict.get)
-        max_cal = dict[Elf_with_max_cal]
-        print(f"Elf number {Elf_with_max_cal} is carrying the most Calories, and it's {max_cal} Calories.")
+        top_tree = []
+        for i in range(3):
+            Elf_with_max_cal = max(dict, key=dict.get)
+            max_cal = dict[Elf_with_max_cal]
+            if i == 0:
+                print(f"Elf number {Elf_with_max_cal} is carrying the most Calories, and it's {max_cal} Calories.")
+            top_tree.append(max_cal)
+            dict.pop(Elf_with_max_cal)
+            if i == 2:
+                print(f"sum of top tree: {sum(top_tree)}")
+            i+=1
 
 
 def get_input():
