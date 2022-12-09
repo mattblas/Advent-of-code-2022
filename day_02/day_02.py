493756d7e2146aecf883009e7ea16709f83b8238
mattblas <matt.blaszkowski@gmail.com>
2022-12-09 09:34:24 +0100

    maintenance

--- day_02/day_02.py
+++ day_02/day_02.py
@@ -1,9 +1,44 @@
-def main():
-    # X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. 
-    # 1 for Rock, 2 for Paper, and 3 for Scissors
-    # A for Rock, B for Paper, and C for Scissors
-    # X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
+# X for Rock, Y for Paper, and Z for Scissors
+# A for Rock, B for Paper, and C for Scissors
 
+
+def main():
+    print(f"Total score (part one): {part_one()}")
+    print(f"Total score (part two): {part_two()}")
+
+
+
+def part_one():
+    choice_points = {
+        "X": 1,
+        "Y": 2,
+        "Z": 3
+    }
+
+    outcome = {
+        "AX": 3,
+        "AY": 6,
+        "AZ": 0,
+        "BX": 0,
+        "BY": 3,
+        "BZ": 6,
+        "CX": 6,
+        "CY": 0,
+        "CZ": 3,
+    }   
+
+    score = []
+    with open("Strategy_guide.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            round_score = outcome[line.replace(" ", "").strip()]
+            choice_score = choice_points[line.replace(" ", "").strip()[1]]
+            score.append(round_score)
+            score.append(choice_score)
+    return sum(score)
+
+
+def part_two():
     choice = {
         #Rock
         "AX": 3, #Sci lose
@@ -24,11 +59,9 @@ def main():
         "AX": 0, 
         "AY": 3,
         "AZ": 6,
-        #Paper
         "BX": 0,
         "BY": 3,
         "BZ": 6,
-        #Sci
         "CX": 0,
         "CY": 3,
         "CZ": 6,
@@ -42,8 +75,7 @@ def main():
             choice_results = choice[line.strip().replace(" ", "")]
             score.append(round_result)
             score.append(choice_results)
-
-    print(f"total score: {sum(score)}")            
+    return sum(score)         
 
 
 if __name__ == "__main__":
