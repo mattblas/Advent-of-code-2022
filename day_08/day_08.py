56226c02961501b8a3f63b30e45a1a68e73f78b1
mattblas <matt.blaszkowski@gmail.com>
2022-12-31 17:14:01 +0100

    day 08 part two

--- day_08/day_08.py
+++ day_08/day_08.py
@@ -1,8 +1,13 @@
-input = "test_input.txt"
+input = "input.txt"
 
 
 def main():
     columns = get_columns()
+    print(part_one(columns))
+    print(part_two(columns))
+
+
+def part_one(columns):
     i = 0
     with open(input, "r") as f:
         for row, line in enumerate(f):
@@ -12,7 +17,47 @@ def main():
                         if check_row(line, column) == False or check_column(line, column, row, columns) == False:
                             i+=1
     answer_part1 = i + (lenght_of_row()*2) + ((len(get_columns())*2)-4)
-    print(f"answer --- part one: {answer_part1}")      
+    return(f"answer --- part one: {answer_part1}")
+
+
+def part_two(columns):
+    all_trees_scores = []
+    with open(input, "r") as f:
+        for row, line in enumerate(f):   
+            for column in range(len(line.strip())):
+                x = check(row, column, line, columns)
+                all_trees_scores.append(x)
+    answer_part2 = max(all_trees_scores)
+    return(f"answer --- part two: {answer_part2}")      
+
+
+def check(row, column, line, columns):
+    list_right = list(line[column+1:len(line.strip())])
+    list_left = list(line[0:column])
+    list_left.reverse()
+    list_up = list(columns[column][0:row])
+    list_up.reverse()
+    list_down = columns[column][row+1:len(columns[column])]
+    x = how_many_visable(list_right, line[column])
+    y = how_many_visable(list_left, line[column])
+    z = how_many_visable(list_up, line[column])
+    v = how_many_visable(list_down, line[column])
+    try:
+        score = x*y*z*v
+    except TypeError:
+        score = 0
+    return score
+
+
+def how_many_visable(list, tree):
+    i = 0
+    for _ in range(len(list)):
+        if tree <= list[_]:
+            i+=1
+            break
+        elif tree > list[_]:
+            i+=1
+    return i
 
 
 def get_columns():
